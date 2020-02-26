897f56345d7f2af079b2c965e61dcea3be5abfaa
Evie Hilton <mhilton@unca.edu>
2020-02-26 16:36:01 -0500

    Add asteroids and player to model

--- src/app/Model.java
+++ src/app/Model.java
@@ -1,7 +1,19 @@
 package app;
 
 public class Model {
+	private int asteroidCount;
+	private Asteroid[] asteroids;
+	private Player player;
+	
     public Model() {
+    	asteroidCount=0;
+    	asteroids = new Asteroid[5];
         
+    }
+    
+    public void addAsteroids() {
+    	if(asteroidCount < 5) {
+    		asteroidCount++;
+    	}
     }
 }
